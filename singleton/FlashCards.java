8110b160fbc1daafb1517e8fde1402029ba8f467
pakshpatel1@gmail.com <pakshpatel1@gmail.com>
2024-02-10 20:59:20 -0500

    commit w/ commetns

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,16 +1,32 @@
 import java.util.*;
 
+/**
+ * Singleton class to manage flash cards and provide a random word
+ * from the list of words.
+ * 
+ * @author Paksh Patel
+ */
 public class FlashCards {
     private Random rand;
     private static FlashCards flashCards;
     private ArrayList<Word> words;
     private Word currentWord;
 
+    /**
+     * Private constructor to prevent instantiation from other classes.
+     * Initializes the random number generator and reads the words from the file.
+     * 
+     */
     private FlashCards() {
         rand = new Random();
         words = FileReader.getWords();
     }
 
+    /**
+     * Static method to get the instance of the FlashCards class.
+     *
+     * @return the instance of the FlashCards class
+     */
     public static FlashCards getInstance() {
         if (flashCards == null) {
             flashCards = new FlashCards();
@@ -18,6 +34,11 @@ public class FlashCards {
         return flashCards;
     }
 
+    /**
+     * Method to get a random word from the list of words.
+     *
+     * @return the random word
+     */
     public Word getWord() {
         if (words == null || words.isEmpty()) {
             System.out.println("No words available");
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,9 +1,23 @@
+/**
+ * Public class to represent a word.
+ * It contains the word, type, definition, and a sentence using the word.
+ * 
+ * @author Paksh Patel
+ */
 public class Word {
     private String word;
     private String type;
     private String definition;
     private String sentence;
 
+    /**
+     * Constructor to create a word object.
+     *
+     * @param word the word
+     * @param type the type of the word
+     * @param definition the definition of the word
+     * @param sentence a sentence using the word
+     */
     public Word(String word, String type, String definition, String sentence) {
         this.word = word;
         this.type = type;
@@ -11,10 +25,20 @@ public class Word {
         this.sentence = sentence;
     }
 
+    /**
+     * Method to get the word.
+     *
+     * @return the word
+     */
     public String getWord() {
         return word;
     }
 
+    /**
+     * Method to get details of the word, including type, definition, and sentence.
+     * 
+     * @return the details of the word
+     */
     public String getDescription() {
         return "Type: " + type + "\nDefinition: " + definition + "\nSentence: " + sentence;
     }
